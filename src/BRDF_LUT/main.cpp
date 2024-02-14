d17220d650b2d88e94d9a4cde7ad975b0b37dfd1
mellic03 <mellic03@gmail.com>
2024-02-14 12:26:31 +1000

    Added BRDF LUT generation

---
+++ src/BRDF_LUT/main.cpp
@@ -0,0 +1,54 @@
+#include <libidk/idk_gl.hpp>
+#include <libidk/idk_gl_headless.hpp>
+#include <libidk/idk_image.hpp>
+
+#include <fstream>
+
+
+#define LUT_TEXTURE_SIZE 128
+
+
+int main()
+{
+    idk::initHeadlessGLContext(4, 6);
+
+    idk::RawImageHeader header = {
+        .major    = 1,
+        .minor    = 1,
+        .format   = idk::IDK_IMAGEFORMAT_F32,
+        .layout   = idk::IDK_IMAGELAYOUT_RG,
+        .w        = LUT_TEXTURE_SIZE,
+        .h        = LUT_TEXTURE_SIZE
+    };
+
+    header.nbytes = idk::RawImage_nbytes(header);
+
+    idk::glTextureConfig config = {
+        .internalformat = GL_RG32F,
+        .format         = GL_RG,
+        .minfilter      = GL_LINEAR,
+        .magfilter      = GL_LINEAR,
+        .wrap_s         = GL_CLAMP_TO_EDGE,
+        .wrap_t         = GL_CLAMP_TO_EDGE,
+        .datatype       = GL_FLOAT,
+        .genmipmap      = GL_FALSE
+    };
+
+
+    GLuint texture = idk::gltools::genTexture2D(header.w, header.h, config);
+    idk::gl::bindImageTexture(0, texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RG32F);
+
+    idk::glShaderProgram program("brdf-lut.comp");
+    program.bind();
+    program.dispatch(LUT_TEXTURE_SIZE / 8);
+    idk::gl::memoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+    program.unbind();
+
+    void *pixels = std::malloc(header.nbytes);
+    idk::gl::getTextureImage(texture, 0, GL_RG, GL_FLOAT, header.nbytes, pixels);
+    idk::RawImage_write("brdf.lut", header, pixels);
+    std::free(pixels);
+
+
+    return 0;
+}
